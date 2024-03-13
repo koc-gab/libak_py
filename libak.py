ce13d061f980bb79c8f11694b2f6dfe0ac137d56
kGábor <65279911+koc-gab@users.noreply.github.com>
2024-03-13 12:04:09 +0100

    libak

--- libak.py
+++ libak.py
@@ -21,10 +21,31 @@ def megszamolas(l):
             db+=1
     return db
 
-def kiir(l, rmog, ra):
+def eldontes1(l):
+    van=False
+    i=0
+    while i<len(l) and not l[i]>=3:
+        i+=1
+    if i<len(l):
+        van=True
+    return van
+
+def eldontes2(l):
+    i=len(l-1)
+    while i>0 and not (l[i]>l[i-1]):
+        i-=1
+    if i<len(l):
+        van=True
+    else:
+        van=False
+    return van
+    
+def kiir(l, rmog, ra, rlhs, rlklens):
     print(f"A libák súlyai: {l}")
     print(f"{rmog}kg libát ehet meg a róka.")
     print(f"Átlagosan {ra}kg-os libákat eszik a róka.")
+    print(f"{rlhs}, hogy a róka 3kg-os libát lopott.")
+    print(f"{rlklens}, hogy a róka kisebb libát hozott, mint az előző napon.")
 
 # FőPROGRAM #
 #Input
@@ -33,5 +54,15 @@ libak=beker()
 r_megehet_ossz_kg=osszegzes(libak)
 r_megehet_db=megszamolas(libak)
 r_atlag=r_megehet_db/r_megehet_db
+r_legalabb_3=eldontes1(libak)
+if r_legalabb_3:
+    r_legalabb_3_str="Előfordult"
+else:
+    r_legalabb_3_str="Nem fordult elő"
+r_lopott_kisebb_libat_elozo_nap=eldontes2(libak)
+if r_lopott_kisebb_libat_elozo_nap:
+    r_lopott_kisebb_libat_elozo_nap_string="Előfordult"
+else:
+    r_lopott_kisebb_libat_elozo_nap_string="Nem fordult elő"
 #Output
-kiir(libak, r_megehet_ossz_kg, r_atlag)
+kiir(libak, r_megehet_ossz_kg, r_atlag, r_legalabb_3_str, r_lopott_kisebb_libat_elozo_nap_string)
